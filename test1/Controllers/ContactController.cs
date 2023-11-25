e4d9707706219b233e79d11b2b8102a7f4c58fbd
qwerkh <narongsao98@gmail.com>
2023-11-25 15:13:03 +0700

    first commit

---
+++ test1/Controllers/ContactController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace test1.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult MyContact()
+        {
+            return View();
+        }
+        public ViewResult Detail()
+        {
+            return View("Details");
+        }
+
+        public ContentResult showContent()
+        {
+            return Content("Hello");
+        }
+        public int add()
+        {
+            int a = 4;
+            int b = 5;
+            int s = a + b;
+            return s;
+        }
+    }
+}
---
+++ test1/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace test1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        //Same Name
+        public ActionResult Index()
+        {
+            ViewBag.name = "Show you Home Page";
+            return View();
+        }
+        //Different Name
+        public ActionResult ShowInfo()
+        {
+            return View("Show");
+        }
+        //In Share Folder
+        public ActionResult Detail()
+        {
+            return View("Details");
+        }
+        //Different Controller
+        public ActionResult ShowMoreDetail()
+        {
+            return View("~/Views/About/MyProfile.cshtml");
+        }
+
+    }
+}
