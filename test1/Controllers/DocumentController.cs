9d177d703ff892e1a5b2b4eb39fde853c4ef70e1
qwerkh <narongsao98@gmail.com>
2023-12-02 02:13:43 +0700

    add controller document

---
+++ test1/Controllers/DocumentController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace test1.Controllers
+{
+    public class DocumentController : Controller
+    {
+        // GET: Document
+        public ActionResult Show()
+        {
+            return File(Server.MapPath("~/Content/test.txt"),"text/plain");
+        }
+        public FileResult Download()
+        {
+            return File(Server.MapPath("~/Content/test.txt"), "text/plain","myfile.txt");
+        }
+
+    }
+}
