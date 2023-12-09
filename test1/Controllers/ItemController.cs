1713be15470401510e454dce582252fa99b17587
qwerkh <narongsao98@gmail.com>
2023-12-09 15:14:22 +0700

    test with model

--- test1/Controllers/ItemController.cs
+++ test1/Controllers/ItemController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using test1.Models;
 
 namespace test1.Controllers
 {
@@ -11,7 +12,18 @@ namespace test1.Controllers
         // GET: Item
         public ActionResult Index()
         {
-            return View();
+            ViewBag.shopName = "Computer Shop";
+            List<ItemList> itemList = new List<ItemList>()
+            {
+                new ItemList{Id="001",Name="Acer",Price=500,Category="Laptop"},
+                new ItemList{Id="002",Name="Mac",Price=1500,Category="Laptop"},
+                new ItemList{Id="003",Name="Asus",Price=600,Category="Laptop"},
+                new ItemList{Id="004",Name="Dell",Price=580,Category="Laptop"},
+            };
+
+            itemList.Add(new ItemList { Id = "005", Name = "MSI", Price = 900, Category = "Laptop" });
+
+            return View(itemList);
         }
     }
 }
